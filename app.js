52b66b8c96d361235da418bc7659fa15d25a8e9f
Danny de Vries <vriesdan@gmail.com>
2017-04-11 15:59:39 +0200

    :wrench: add express base structure

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var express = require('express');
 
+var port = process.env.PORT || '3000';
+var host = process.env.HOST || '0.0.0.0';
+
 var app = express();
+
+app.use(express.static('src'))
+  .set('views', 'views');
+
+app.listen(port, host, function () {
+  console.log('Server running', host, ':', port);
+});
