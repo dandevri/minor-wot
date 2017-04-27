42f87bc4c5a5856123202fcabbd8841391ef2be5
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-04-27 14:32:52 +0200

    :sparkles: add volume manipulation
    Signed-off-by: Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Set dependencies
 var Spotify = require('spotify-web-api-node');
 var express = require('express');
 var querystring = require('querystring');
@@ -8,53 +9,79 @@ var includes = require('lodash/includes');
 var map = require('lodash/map');
 var fetch = require('node-fetch');
 
+// Set port and host variables
 var port = process.env.PORT || '3000';
 var host = process.env.HOST || '0.0.0.0';
 
+// Set Spotify oAuth credentials and state_key
 var CLIENT_ID = process.env.CLIENT_ID;
 var CLIENT_SECRET = process.env.CLIENT_SECRET;
 var REDIRECT_URI = process.env.REDIRECT_URI;
 var STATE_KEY = 'spotify_auth_state';
 
+// Define scopes for API endpoint data
 var scopes = ['user-read-currently-playing', 'user-modify-playback-state'];
 
+// Call the spotify-web-api-node package and set credentials
 var spotifyApi = new Spotify ({
   clientId: CLIENT_ID,
   clientSecret: CLIENT_SECRET,
   redirectUri: REDIRECT_URI
 });
 
-var generateRandomString = N => (Math.random().toString(36)+Array(N).join('0')).slice(2, N+2);
+var generateRandomString = function(N) {
+  return (Math.random().toString(36)+Array(N).join('0')).slice(2, N+2);
+};
 
+// Set default volume
 var votes = [];
 var volume = 50;
 
+// Set express app and declare all functions
 var app = express();
 app.use(express.static('src'))
-  .use(cookieParser())
-  .set('views', 'views')
-  .set('view engine', 'ejs');
+   .use(cookieParser())
+   .set('views', 'views')
+   .set('view engine', 'ejs')
+   .get('/', home)
+   .get('/login', login)
+   .get('/callback', callback)
+   .get('/current', getUsersCurrentlyPlayingTrack)
+   .post('/play', startUsersPlayback)
+   .post('/pause', pauseUsersPlayback)
+   .post('/next', nextUsersTrack)
+   .post('/previous', previousUsersTrack)
+   .post('/volume', setCurrentUsersVolume)
+   .get('/refresh-token', refreshToken)
+   .get('/api/vote', apiVote);
 
-app.get('/', function (req, res) {
+// Get home view
+function home (req, res) {
   res.render('index');
-});
+}
 
-app.get('/login', function (req, res) {
+function login (req, res) {
+  // Generate a random state key
   var state = generateRandomString(16);
+  // Set HTTP header with the STATE_KEY and generated string
   res.cookie(STATE_KEY, state);
+  // Create authorization URL with the scopes and state in order to receive correct data
   res.redirect(spotifyApi.createAuthorizeURL(scopes, state));
-});
+}
 
-app.get('/callback', function (req, res) {
+function callback (req, res) {
   const { code, state } = req.query;
-  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+  var storedState = req.cookies ? req.cookies[STATE_KEY] : null;
 
+// First do state validation
   if (state === null || state !== storedState) {
      res.redirect('/#' +
        querystring.stringify({
          error: 'state_mismatch'
        }));
+  // If the state is valid, get the authorization code and pass it on the client
   } else {
+     // Clear the cookie that was set
      res.clearCookie(STATE_KEY);
      // Retrieve an access token and a refresh token
      spotifyApi.authorizationCodeGrant(code).then(data => {
@@ -64,62 +91,81 @@ app.get('/callback', function (req, res) {
        spotifyApi.setAccessToken(access_token);
        spotifyApi.setRefreshToken(refresh_token);
 
-       // use the access token to access the Spotify Web API
-       spotifyApi.getMe().then(({ body }) => {
-         console.log(body);
-       });
-
-       // we can also pass the token to the browser to make requests from there
-       res.redirect('/current/#' +
-         querystring.stringify({
-           access_token: access_token,
-           refresh_token: refresh_token
-         }));
-     }).catch(err => {
-       res.redirect('/#' +
-         querystring.stringify({
-           error: 'invalid_token'
-         }));
+       // Redirect to current page
+       res.redirect('/current');
      });
    }
-});
+}
 
-app.get('/current', function (req, res) {
+// Get the object currently being played on the user's Spotify account
+function getUsersCurrentlyPlayingTrack (req, res) {
    spotifyApi.getUsersCurrentlyPlayingTrack().then(({ body }) => {
-     console.log(body);
+     // Render the data to the current view
      res.render('current', { data : body});
    });
- });
+ }
 
-app.post('/play', function (req, res) {
+// Start a new context or resume current playback on the user's active device
+function startUsersPlayback (req, res) {
   spotifyApi.startUsersPlayback().then(({ body }) => {
-    console.log(body);
     res.redirect('/current');
   });
-});
+}
 
-app.post('/pause', function (req, res) {
+// Pause playback on the user's account
+function pauseUsersPlayback (req, res) {
   spotifyApi.pauseUsersPlayback().then(({ body }) => {
-    console.log(body);
     res.redirect('/current');
   });
-});
+}
 
-app.post('/next', function (req, res) {
+// Skips to previous track in user's queue
+function nextUsersTrack (req, res) {
   spotifyApi.nextUsersTrack().then(({ body }) => {
-    console.log(body);
     res.redirect('/current');
   });
-});
+}
 
-app.post('/previous', function (req, res) {
+// Skip to user's previous track in queue
+function previousUsersTrack (req, res) {
   spotifyApi.previousUsersTrack().then(({ body }) => {
-    console.log(body);
     res.redirect('/current');
   });
-});
+}
 
-app.get('/refresh_token', function (req, res) {
+// Set the volume for the user’s current playback device
+function setCurrentUsersVolume (req, res) {
+  spotifyApi.setCurrentUsersVolume(100).then(({ body }) => {
+    console.log('Received volume ', req.query.direction, ' request');
+    if (req.query.direction === 'up') {
+      volume += 10;
+    } else if (req.query.direction === 'down') {
+      volume -= 10;
+    } else {
+      console.warn('Unknown query passed');
+    }
+
+    if (volume > 100) {
+      volume = 100;
+    } else if (volume < 0) {
+      volume = 0;
+    }
+    res.redirect('/current');
+    // res.send('Volume will be set to ' + volume);
+  });
+}
+
+function removeSettings(id) {
+  setTimeout(
+    function () {
+      fetch('http://oege.ie.hva.nl/~palr001/icu/api.php?t=rdc&d=XXXX&td=' + id);
+    },
+    4000
+  );
+}
+
+// Request access token from refresh token
+function refreshToken (req, res) {
   const { refresh_token } = req.query;
   if (refresh_token) {
     spotifyApi.setRefreshToken(refresh_token);
@@ -131,9 +177,9 @@ app.get('/refresh_token', function (req, res) {
   }).catch(err => {
     console.log('Could not refresh access token', err);
   });
-});
+}
 
-app.get('/api/vote', function (req, res) {
+function apiVote (req, res) {
   console.log('Received vote from ', req.query.chip);
   if (!includes(votes, req.query.chip)) {
     buttonFeedback('vote', req.query.chip);
@@ -142,7 +188,7 @@ app.get('/api/vote', function (req, res) {
     return;
   }
   res.send(req.query.chip + ' has already voted');
-});
+}
 
 function vote(id) {
   votes.push(id);
@@ -190,39 +236,11 @@ function buttonFeedback(type, id, all) {
   }
 }
 
-app.get('/api/volume', function (req, res) {
-  console.log('Received volume ', req.query.direction, ' request');
-  if (req.query.direction === 'up') {
-    volume += 10;
-  } else if (req.query.direction === 'down') {
-    volume -= 10;
-  } else {
-    console.warn('Unknown query passed');
-  }
-
-  if (volume > 100) {
-    volume = 100;
-  } else if (volume < 0) {
-    volume = 0;
-  }
-
-  res.send('Volume will be set to ' + volume);
-});
-
-function removeSettings(id) {
-  setTimeout(
-    function () {
-      fetch('http://oege.ie.hva.nl/~palr001/icu/api.php?t=rdc&d=XXXX&td=' + id);
-    },
-    4000
-  );
-}
-
 function clearVotes() {
   votes = [];
   return votes;
 }
 
 app.listen(port, host, function () {
-  console.log('Server running', host, ':', port);
+  console.log('Server running on', host, ':', port);
 });
